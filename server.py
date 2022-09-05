502ffe758b9427911e7ed44c19bdf0e24ff9f0fc
nitzan-karni <51075167+nitzan-karni@users.noreply.github.com>
2022-09-05 17:21:34 +0300

    server

---
+++ server.py
@@ -0,0 +1,78 @@
+from flask import request
+from waitress import serve
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+from flask_cors import CORS
+import json
+
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+app.debug = True
+app.config['SECRET KEY'] = 'secret!'
+CORS(app)
+socket = SocketIO(app, cors_allowed_origins='*')
+
+
+@socket.on('connect')
+def connect():
+    print('\n *client connected* \n')
+
+
+@socket.on('disconnect')
+def disconnect():
+    print('\n *client disconnected* \n')
+
+
+@socket.on('post_message')
+def post_message(new_message):
+    with open(r'C:\Users\user\Desktop\Chat\server\data.json', 'r+') as data_file:
+        data = json.load(data_file)
+
+        print(new_message)
+        print(type(new_message))
+
+        data['messages'].append(new_message)
+        data_file.seek(0)
+        json.dump(data, data_file, indent=4)
+
+        # rest of the clients should request the messages again
+        socket.emit('refresh', broadcast=True)
+
+        return request.data
+
+
+@socket.on('get_messages')
+def get_messages():
+    data_file = open(r'C:\Users\user\Desktop\Chat\server\data.json')
+    data = json.load(data_file)
+    return data
+
+
+@app.route('/')
+def index():
+    # return app.send_static_file('index.htm')
+    return 'hello this is chat app server'
+
+
+# @app.route("/messages", methods=['GET', 'POST'])
+# def messages(msg=None):
+#     if request.method == 'GET':
+#         data_file = open(r'C:\Users\user\Desktop\Chat\server\data.json')
+#         data = json.load(data_file)
+#         return data
+
+#     if request.method == 'POST':
+#         with open(r'C:\Users\user\Desktop\Chat\server\data.json', 'r+') as data_file:
+#             data = json.load(data_file)
+
+#             new_message = json.loads(request.data)
+#             print(new_message)
+#             print(type(new_message))
+
+#             data['messages'].append(new_message)
+#             data_file.seek(0)
+#             json.dump(data, data_file, indent=4)
+#             send('REFRESH', broadcast=True)
+#             return request.data
+
+if __name__ == '__main__':
+    socket.run(app, host='0.0.0.0', port=5000)
